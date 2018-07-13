0f6e2a031b89d44cb1f0a84d79fd565b09f00be6
protatodev <thaddonaghue@yahoo.com>
2018-07-13 15:42:28 -0700

    add further controller testing

---
+++ HairSalon.Tests/ControllerTests/ClientControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests.ControllerTests
+{
+    [TestClass]
+    public class ClientControllerTest
+    {
+        [TestMethod]
+        public void Create_ReturnsCorrectView_True()
+        {
+            ClientController controller = new ClientController();
+
+            ActionResult createView = controller.Create();
+
+            Assert.IsInstanceOfType(createView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Details_ReturnsCorrectView_True()
+        {
+            ClientController controller = new ClientController();
+
+            ActionResult detailsView = controller.Details(1);
+
+            Assert.IsInstanceOfType(detailsView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Update_ReturnsCorrectView_True()
+        {
+            ClientController controller = new ClientController();
+
+            ActionResult updateView = controller.Update(1);
+
+            Assert.IsInstanceOfType(updateView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void ViewAll_ReturnsCorrectView_True()
+        {
+            ClientController controller = new ClientController();
+
+            ActionResult viewAllView = controller.ViewAll();
+
+            Assert.IsInstanceOfType(viewAllView, typeof(ViewResult));
+        }
+    }
+}
---
+++ HairSalon.Tests/ControllerTests/StylistControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.Tests.ControllerTests
+{
+    [TestClass]
+    public class StylistControllerTest
+    {
+        [TestMethod]
+        public void Create_ReturnsCorrectView_True()
+        {
+            StylistController controller = new StylistController();
+
+            ActionResult createView = controller.Create();
+
+            Assert.IsInstanceOfType(createView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Details_ReturnsCorrectView_True()
+        {
+            StylistController controller = new StylistController();
+
+            ActionResult detailsView = controller.Details(1);
+
+            Assert.IsInstanceOfType(detailsView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Update_ReturnsCorrectView_True()
+        {
+            StylistController controller = new StylistController();
+
+            ActionResult updateView = controller.Update(1);
+
+            Assert.IsInstanceOfType(updateView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void ViewAll_ReturnsCorrectView_True()
+        {
+            StylistController controller = new StylistController();
+
+            ActionResult viewAllView = controller.ViewAll();
+
+            Assert.IsInstanceOfType(viewAllView, typeof(ViewResult));
+        }
+    }
+}
